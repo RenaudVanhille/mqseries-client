cddd82d39987eec0bf0f4992e0e6752ccef0bf62
RenaudVanhille <renaudvanhille@gmail.com>
2017-09-05 19:45:18 +0200

    init(initialisation): pub

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const pub = require('./build/Release/mq_pub.node');
-
+console.log(pub);
 function SendPub(i) {
         return new Promise(
                 (resolve, reject) => {
-                        pub.send('message ' + i).then(
+                        pub.publish('message ' + i).then(
                                 () => { setTimeout(() => { return SendPub(++i); }, 1000) },
                                 (reason) => { reject(reason); }
                         );
@@ -11,6 +11,14 @@ function SendPub(i) {
         );
 }
 
-pub.init('QM2', 'sport').then(() => {
+pub.init('QM1', 'sport').then(() => {
     return SendPub(0).then(console.log, console.error)
     }, console.error);
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Salut tout le monde !');
+});
+server.listen(8080);
